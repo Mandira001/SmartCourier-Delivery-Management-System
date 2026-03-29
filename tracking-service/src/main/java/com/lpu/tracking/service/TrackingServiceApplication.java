4c05de7ce19f812fc0f16742b618ff801fa82667
Mandira Nagar <nagarmandira27@gmail.com>
2026-03-29 16:01:28 +0530

    till swagger done

--- tracking-service/src/main/java/com/lpu/tracking/service/TrackingServiceApplication.java
+++ tracking-service/src/main/java/com/lpu/tracking/service/TrackingServiceApplication.java
@@ -3,9 +3,11 @@ package com.lpu.tracking.service;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableRabbit
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TrackingServiceApplication {
 
 	public static void main(String[] args) {
